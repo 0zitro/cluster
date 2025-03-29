599167f7c4ad0911e77cc84d504cef8e16f2aa44
zitro <94910351+0zitro@users.noreply.github.com>
2025-03-29 19:46:59 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 
 VPS_IP=$(curl -4 ifconfig.me)
 K8S_VERSION="1.32"
-# CILIUM_VERSION="1.17.2"
+CALICO_VERSION="3.29.3"
 CERTMANAGER_VERSION="1.17.1"
+
 
 # Disable swap
 swapoff -a
@@ -12,7 +13,6 @@ sed -i '/ swap / s/^/#/' /etc/fstab
 # Add required modules, persist them in modules-load.d
 echo "overlay" > /etc/modules-load.d/k8s-modules.conf
 echo "br_netfilter" >> /etc/modules-load.d/k8s-modules.conf
-
 modprobe overlay
 modprobe br_netfilter
 
@@ -30,11 +30,7 @@ apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold
 apt-get install -y apt-transport-https ca-certificates curl gpg
 
 # Keyrings:
-# If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
 mkdir -p -m 755 /etc/apt/keyrings
-
-#  - Docker:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 #  - Kubernetes:
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v${K8S_VERSION}/deb/Release.key | gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring_v${K8S_VERSION}.gpg
 #  - Helm:
@@ -43,27 +39,10 @@ curl https://baltocdn.com/helm/signing.asc | gpg --dearmor --yes -o /etc/apt/key
 chmod -R a+r /etc/apt/keyrings
 
 # Repos:
-#  - Docker:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" > /etc/apt/sources.list.d/docker.list
 #  - Kubernetes:
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring_v${K8S_VERSION}.gpg] https://pkgs.k8s.io/core:/stable:/v${K8S_VERSION}/deb/ /" > /etc/apt/sources.list.d/kubernetes.list
 #  - Helm:
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" > /etc/apt/sources.list.d/helm-stable-debian.list
-
-# use systemd for the management of the container’s cgroups
-# mkdir -p /etc/docker
-# cat <<EOF | sudo tee /etc/docker/daemon.json
-# {
-#   "exec-opts": ["native.cgroupdriver=systemd"],
-#   "log-driver": "json-file",
-#   "log-opts": {
-#     "max-size": "100m"
-#   },
-#   "storage-driver": "overlay2"
-# }
-# EOF
 
 # install tools
 apt-get -y update
@@ -72,8 +51,7 @@ apt-mark hold kubelet kubeadm kubectl
 
 # Reset previous install
 kubeadm reset -f
-# rm -f /etc/cni/net.d/05-cilium.conflist
-rm -f /etc/cni/net.d/.kubernetes-cni-keep
+rm -rf /etc/cni/net.d
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 
 # Reset configs and restart services
@@ -93,29 +71,13 @@ mkdir -p $HOME/.kube
 cp /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-# # install networking model
-# helm repo add cilium https://helm.cilium.io/
-# helm install cilium cilium/cilium --version ${CILIUM_VERSION} --namespace kube-system
-# Use calico
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/calico.yaml
+# Use calico as the networking model
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml
 
-# install ingress
+# Install ingress
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-# helm install ingress-nginx ingress-nginx/ingress-nginx
-# kubectl patch svc ingress-nginx-controller -p "{\"spec\": {\"type\": \"LoadBalancer\", \"externalIPs\":[\"${VPS_IP}\"]}}"
-
 helm install nginx-ingress ingress-nginx/ingress-nginx \
   --namespace ingress-nginx --create-namespace \
   --set controller.hostNetwork=true \
   --set controller.kind=DaemonSet \
   --set controller.service.type=ClusterIP
-
-# # install certificate manager
-# helm repo add jetstack https://charts.jetstack.io
-# helm repo update
-# helm install \
-#   cert-manager jetstack/cert-manager \
-#   --namespace cert-manager \
-#   --create-namespace \
-#   --version v${CERTMANAGER_VERSION} \
-#   --set installCRDs=true
